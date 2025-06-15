a308e398387bb523af074df2b5833efca9acd2ad
KekichSergich <nikitakild.0989@gmail.com>
2025-06-15 18:10:28 +0200

    add flame's changes and uncomment  serviceWorker

--- js/utils/rocketControl.js
+++ js/utils/rocketControl.js
@@ -4,18 +4,35 @@ function closeModal() {
     }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const svg = document.getElementById('rocketSVG');
-    svg.addEventListener('click', () => {
+  const svg = document.getElementById('rocketSVG');
+  const flame = document.getElementById('flame');
+  const flameColors = ['orange', '#00bfff', '#ff4500', '#ffa500'];
+  let flameIndex = 0;
+
+  function cycleFlameColor() {
+    flameIndex = (flameIndex + 1) % flameColors.length;
+    flame.setAttribute('fill', flameColors[flameIndex]);
+  }
+
+  svg.addEventListener('click', () => {
+    setInterval(cycleFlameColor, 100);
     let offset = 0;
     const anim = setInterval(() => {
-        offset -=10;
-        svg.style.transform = `translateY(${offset}px)`;
-        svg.style.transition = 'transform 0.03s linear';
-        if (offset < -220) {
+      offset -= 10;
+      svg.style.transform = `translateY(${offset}px)`;
+      svg.style.transition = 'transform 0.03s linear';
+      if (offset < -220) {
         clearInterval(anim);
         svg.remove();
         closeModal();
-        }
+      }
     }, 30);
-    });
+  });
 });
+
+
+
+
+
+
+
